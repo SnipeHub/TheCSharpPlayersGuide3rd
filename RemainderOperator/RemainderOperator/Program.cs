c14a80b3f24e1c06b6ec4af2a495ff7db0c97425
SnipeHub <v-macar2@microsoft.com>
2018-07-11 20:41:13 +1000

    some operators

---
+++ RemainderOperator/RemainderOperator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemainderOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            remainders work like this.For 23 % 7, we know that 7 goes in to 23 a total of (% means remainder in C#)
+            3 whole times. But since 7 * 3 is 21, there will be two left over. In this case, 2 is our remainder.
+            */
+
+            int totalApples = 23;
+            int people = 7;
+            int remainingApples = totalApples % people;  // this will be 2
+
+            Console.WriteLine("23 apples divided by 7, the remainder is: " + remainingApples);
+
+            // For example, the remainder operator can be used to determine if a value is a multiple of another value. If it is, then the remainder operator weill give us 0.
+            int totalCars = 20;
+            int carBuyers = 4;
+            int remainingCars = totalCars % carBuyers;
+            Console.WriteLine("Cars unsold: " + remainingCars);
+
+            // try it out section
+            int a = 17;
+            int b = 4;
+            int quotient = a / b; // our division
+            int remainder = a % b; // our remainder
+            int extraAddition = b * quotient + remainder;
+
+            Console.WriteLine(a + " divided by " + b + " is: " + quotient + ", remainder is: " + remainder);
+            Console.WriteLine("Original total of variable a: " + extraAddition);
+        }
+    }
+}
---
+++ UnaryOperators/UnaryOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaryOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // These are the unary '+' and '-' operators. They only work on the number right after them.
+            int a = +3; // this is a positive 3
+            int b = -44; // this is a negative 44
+
+            // These are the binary '+' and '-' operators (addition and subtraction) that operate on two numbers.
+            int c = 3 + 4;
+            int d = 2 - 66;
+        }
+    }
+}
