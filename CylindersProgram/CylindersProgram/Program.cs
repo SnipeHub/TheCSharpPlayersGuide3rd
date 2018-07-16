ca08d86961a5e1f9d7824daaa8140b6ade803012
SnipeHub <v-macar2@microsoft.com>
2018-07-16 22:07:31 +1000

    created a cylinder calculator

---
+++ CylindersProgram/CylindersProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CylindersProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pi = 3.1415926f;
+            //volume = pi * radius * radius * height;
+            //surfaceArea = 2 * pi * radius * (radius + height);
+
+            // Our greeting message.
+            Console.WriteLine("Welcome to Cylinder Calculator 1.0!");
+
+            // Read in the cylinder's radius from the user
+            Console.Write("Enter the cylinder's radius: ");
+            string radiusAsAString = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusAsAString);
+
+            // Read in the cyliunder's height from the user
+            Console.Write("Enter the cylinder's height: ");
+            string heightAsAString = Console.ReadLine();
+            double height = Convert.ToDouble(heightAsAString);
+
+            double pi = 3.141592654;
+
+            // Our calculations using the formulas for a cylinder, under geometry
+            // You can find them on Wikipedia: https://en.wikipedia.org/wiki/Cylinder
+            double volume = pi * radius * radius * height;
+            double surfaceArea = 2 * pi * radius * (radius + height);
+
+            // Now we output the results
+            Console.WriteLine("The cylinder's volume is: " + volume + " cubic units.");
+            Console.WriteLine("The cylinder's surface area is: " + surfaceArea + " square units.");
+
+            // wait for the user to respond before closing...
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string whatTheUserTyped = Console.ReadLine(); // to capture user input data and place inside the string variable
+            int aNumber = Convert.ToInt32(whatTheUserTyped); // to convert into an integer
+
+            Console.WriteLine("You typed the following: " + whatTheUserTyped);
+
+            // Other examples
+            bool b = false;
+            int i = Convert.ToInt32(b);
+
+            double d = 3.4;
+            float f = Convert.ToSingle(d);
+        }
+    }
+}
