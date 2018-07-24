577d63a53b84d7a99869423b7a7bf156d1970e6b
SnipeHub <v-macar2@microsoft.com>
2018-07-24 19:33:08 +1000

    Integer division, use float if not wanting to use integer division

---
+++ IntegerDivision/IntegerDivision/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 2;
+            int result = a / b;
+            Console.WriteLine(result); // incorrect result, wheres the remainder??
+
+            float d = 7.0f;
+            float e = 2.0f;
+            float f = d / e;
+            Console.WriteLine(f);
+        }
+    }
+}
