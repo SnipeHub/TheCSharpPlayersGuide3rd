c7613f1e9b397fb0aff8fe35d7c8205497c6e04b
SnipeHub <v-macar2@microsoft.com>
2018-07-31 20:28:47 +1000

    Using bool with if

---
+++ EvenOddUsingIf/EvenOddUsingIf/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddUsingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int scoreOne = 2;
+
+            Console.WriteLine("Please enter a number between 1 & 100: ");
+            string userString = Console.ReadLine();
+            int convertToInt = Convert.ToInt32(userString);
+
+            int remainderCalculation = convertToInt % scoreOne;
+
+            Console.WriteLine($"The remainder calculation of your input of {convertToInt} remainder {scoreOne} is: {remainderCalculation}");
+
+            if (remainderCalculation == 0)
+                Console.WriteLine("The remainder is even");
+            else if (remainderCalculation == 1)
+                Console.WriteLine("The remainder is odd");
+
+            Console.ReadKey();
+                
+
+            
+        }
+    }
+}
---
+++ IfBool/IfBool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfBool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 45; // Ideally, this would change as the player progresses through the game.
+
+            int pointsNeededToPass = 100;
+
+            bool levelComplete;
+
+            if (score >= pointsNeededToPass)
+                levelComplete = true;
+            else
+                levelComplete = false;
+
+            if (levelComplete)
+            {
+                Console.WriteLine("You've beaten the level!");
+            }
+        }
+    }
+}
