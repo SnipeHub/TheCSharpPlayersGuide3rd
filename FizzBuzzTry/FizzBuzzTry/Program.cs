274a94d8a1244bf658211981a07115c701d77f95
SnipeHub <modmapper@hotmail.com>
2018-09-01 19:37:39 +1000

    Nesting Loops
    - also trying to figure out a FizzBuzz which is something I need to figure out, almost there

---
+++ FizzBuzzTry/FizzBuzzTry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTry
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Fizz = 3;
+            int a = 100;
+
+            Console.WriteLine("The FizzBuzz\n");
+
+            // calculations here
+            int checkRemainderFizz = Fizz % a;
+            Console.WriteLine($"{Fizz} into {a} = {checkRemainderFizz}");
+
+            for (int x = 1; x <= 100; x++)
+            {
+                Console.WriteLine(x);
+                for (int z = 3; checkRemainderFizz <= 100;) ;
+                    Console.Write("Fizz");
+
+                
+            }
+
+        }
+    }
+}
---
+++ NestingLoops/NestingLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestingLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int column = 0; column < 5; column++)
+            {
+                for (int row = 0; row < 10; row++)
+                    Console.Write("*");
+
+                Console.WriteLine(); // This makes it wrap around the beginning of the line.
+            }
+
+            for(int row = 0; row < 10; row++)
+            {
+                for (int column = 0; column < row + 1; column++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
