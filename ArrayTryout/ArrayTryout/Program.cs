668d59bb3b1751a225ee25fb57043fc402e2d96a
SnipeHub <modmapper@hotmail.com>
2018-09-11 14:37:28 +1000

    More arrays
    - finding an average from an array
    - copying an array
     more to come

---
+++ ArrayTryout/ArrayTryout/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTryout // tryout Page 106 copying an array http://starboundsoftware.com/books/c-sharp/try-it-out/copying-an-array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = new int[] { 34, 88, 1, 97, 16, 32, 33, 48, 59, 100 };
+            int arrayOneLength = arrayOne.Length;
+
+            // copy the values from one array to a copy
+            int[] arrayOneCopy = new int[arrayOne.Length];
+            for (int index = 0; index < arrayOne.Length; index++)
+            {
+                arrayOneCopy[index] = arrayOne[index];
+            }
+
+            // print out the original array.
+            for (int index = 0; index < arrayOne.Length; index++)
+            {
+                Console.WriteLine($"Original Array: {arrayOne[index]}");
+            }
+            Console.WriteLine(); // writes a blank line, think of it as a new paragraph
+            Console.WriteLine($"The array count is {arrayOneLength} items.");
+            Console.WriteLine();
+
+            // print out the copy.
+            for (int index = 0; index < arrayOneCopy.Length; index++)
+            {
+                Console.WriteLine($"Copy of Array: {arrayOneCopy[index]}");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AverageValueArray/AverageValueArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageValueArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+            int total = 0;
+            int arrayTally = arrayOne.Length; // tells us how many in the array, not needed, just for myself
+
+            for (int index = 0; index < arrayOne.Length; index++)
+            {
+                total += arrayOne[index];
+            }
+            float average = (float)total / arrayOne.Length;
+
+            Console.WriteLine(average); // 11.55556
+            Console.ReadKey();
+        }
+    }
+}
