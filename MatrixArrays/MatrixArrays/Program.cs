d131b63be63af5ef568259f39d12ccf44b27b31f
SnipeHub <modmapper@hotmail.com>
2018-09-13 12:18:52 +1000

    More arrays
    - multi dimensional arrays

--- MatrixArrays/MatrixArrays/Program.cs
+++ MatrixArrays/MatrixArrays/Program.cs
@@ -16,20 +16,22 @@ namespace MatrixArrays
             // matrixOne[3, 3] = 1;
 
             // jagged array
-            int[][] matrixOne = new int[4][];
-            matrixOne[0] = new int[2];
-            matrixOne[1] = new int[6];
-            matrixOne[2] = new int[8];
-            matrixOne[3] = new int[10];
+            int[][] matrixOne = new int[5][];
+            matrixOne[0] = new int[1];
+            matrixOne[1] = new int[2];
+            matrixOne[2] = new int[4];
+            matrixOne[3] = new int[5];
+            matrixOne[4] = new int[8];
 
             // continue filling in values...
 
             for (int row = 0; row < matrixOne.Length; row++)
             {
                 for (int column = 0; column < matrixOne[row].Length; column++)
-                
-                    Console.WriteLine(matrixOne[row][column] + " "); // Each item in the row seperated by spaces
-                Console.WriteLine(); // to put our spaces in the rows
+                {
+                    Console.Write(matrixOne[row][column] + " "); // Each item in the row seperated by spaces Use console.Write rather than writeline.
+                }
+                Console.WriteLine(); // to put our spaces in the rows, otherwise it will write its all on one line.
             }
             
 
---
+++ MatrixArrays2/MatrixArrays2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrayThreeByThree = new int[3, 3] // 3 rows by 3 columns
+            {
+                { 0,1,2},           // row 1
+                { 3,4,5},           // row 2
+                { 6,7,8}            // row 3
+            };
+         // 3 columns
+
+            for (int row = 0; row < arrayThreeByThree.GetLength(0); row++)
+            {
+                for (int column = 0; column < arrayThreeByThree.GetLength(1); column++)
+                {
+                    Console.Write(arrayThreeByThree[row, column] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
