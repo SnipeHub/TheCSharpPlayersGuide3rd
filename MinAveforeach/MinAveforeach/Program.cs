09559b5f4523daa00ea6ba71dcab76ad8da9055c
SnipeHub <modmapper@hotmail.com>
2018-09-16 13:17:12 +1000

    foreach loop
    - much more easier and simple compared to a for loop
    - foreach loops are apparently slower to compute
    - foreach do have a purpose which will be explained in Enumerations

---
+++ MinAveforeach/MinAveforeach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinAveforeach // Tryout for end of chp 13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[10] { 33, 23, 1, 56, 768, 0, 99, 169, 17, 10 };
+
+            int currentMinimum = Int32.MaxValue;
+
+            foreach (int number in scores)
+            {
+                if (number < currentMinimum) // damn, I was close, had a bit too much going on.
+                {
+                    currentMinimum = number;
+                }
+                //Console.WriteLine($"Minimum: {currentMinimum}");
+            }
+            Console.WriteLine($"Minimum: {currentMinimum}"); // place this outside the foreach, so it will give you only the minimum answer. If you oput it inside, it still works, but will print out each min value it goes through
+            Console.WriteLine();
+
+            // determine the average value of the array
+            int total = 0;
+
+            foreach (int number in scores) // I like foreach, a lot easier to work with compared to using for
+            {
+                total += number; // += sum of the total
+            }
+            float average = (float)total / scores.Length; // all numbers totaled and / by how many numbers
+
+            Console.WriteLine($"Average: {average}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ foreachLoop/foreachLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[10] { 33, 23, 1, 56, 768, 0, 99, 169, 17, 10 };
+
+            foreach (int number in scores) // a lot easier to read and simpler than a for loop. Apparently a foreach loop is slower.
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
