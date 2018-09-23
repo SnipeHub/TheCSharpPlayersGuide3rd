25c57f2871bf7dd8f679914dc622ac1a78abf039
SnipeHub <modmapper@hotmail.com>
2018-09-23 17:25:24 +1000

    Passing to methods
    - passing to multiple parameters

---
+++ PassingMultipleMethods/PassingMultipleMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingMultipleMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getMultiply = Multiply(100, 20);
+            Console.WriteLine($"The answer is: {getMultiply}");
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ PassingToMethod/PassingToMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingToMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count(5);
+            
+            Count(15); // we define in the brackets the number of counts
+        }
+
+        // Our number counter. You must alos have an explicit type, you can not use var.
+        static void Count(int numberToCountTo)
+        {
+            for (int current = 1; current <= numberToCountTo; current++)
+                Console.WriteLine(current);
+            Console.WriteLine();
+        }
+    }
+}
