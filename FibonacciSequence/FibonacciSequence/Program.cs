60949685200401f2dc86338a5493f08dcbba449f
SnipeHub <modmapper@hotmail.com>
2018-10-01 16:52:55 +1000

    Fibonacci sequence
    - try out end of chp 15

---
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 1; index <= 10; index++)
+            {
+                Console.WriteLine(Fibonacci(index));
+            }
+
+            Console.ReadKey();
+        }
+
+        static ulong Fibonacci(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            if (number == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+    }
+}
