1ee5f0f381ff25b06762656eea342102100e3c54
SnipeHub <modmapper@hotmail.com>
2018-10-07 14:13:18 +1100

    Value types and reference types chp 16
    difference between value types and reference types.
    - your value types are int, bool etc which are stored as themselves
    - reference types are your variables which go on the heap like string, Arrays

---
+++ NullReferences/NullReferences/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // One interesting thing about reference types is that you can have them reference nothing at all. This is called a null reference. Just assign a value of null.
+            string message = null;
+
+            // we create a new array and assign numbers
+            int[] numbers = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+            // We have now reassigned numbers variable to null. The array is no longer being referecnced, therefore it has become inaccessible, and isolated on the heap. Eventually it will be garbaged collected.
+            numbers = null;
+
+            // your program will crash if you try to do something with it if its value is null.
+            int[] number = null;
+            number[3] = 6; // This crashes, since numbers doesn't reference anything.
+
+            // To address this, you can do a simple check to see if a reference type is null first:
+            if(number != null)
+            {
+                number[3] = 6;
+            }
+
+
+            // try out questions (my answers)
+
+            // 1. False Y
+            // 2. True  Y
+            // 3. True  Y
+            // 4. False Y
+            // 5. True  Y
+            // 6. True  N SHOULD BE FALSE
+            // 7. True  N False
+            // 8. True  Y
+            // 9. False Y
+        }
+    }
+}
---
+++ ReferenceTypeExample/ReferenceTypeExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypeExample // chapter 16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string type is a reference type, the actual "Hello!" text will always be stored in the heap,
+            // and the message variable will simply contain a reference to that text.
+            // As a local variable or parameter, the message variable will be stored on the stack somewhere, 
+            // and contain a reference to that actual text on the heap.
+            string message = "Hello!";
+
+            Console.WriteLine(message);
+
+            ReferenceVariables();
+        }
+
+        static void ReferenceVariables()
+        {
+            // An array of strings, both of which are reference types.
+            string[] messages = new string[3] { "Hello", "World", "!" };
+
+            // if we were to do our array with numbers, they will exist in the variable and not be pointed from elsewhere.
+            int[] numbers = new int[3] { 2, 3, 4 };
+        }
+    }
+}
