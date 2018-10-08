f966d15c003df054c472f11d2b8a5192be6f1daa
SnipeHub <modmapper@hotmail.com>
2018-10-08 20:26:27 +1100

    Random Class
    - also did a random die roll

---
+++ DiceBag/DiceBag/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceBag
+{
+    class Program // Chp 17 Pg 110 try it out! Die Rolling
+    {
+        static void Main(string[] args)
+        {
+            Random randomGen = new Random();
+
+            // Gets the number of die to roll here
+            Console.WriteLine($"Please enter number of 6d dice to roll: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            // Simulate a die roll for each of the dice that were requested.
+            int total = 0;
+            for (int index = 0; index < count; index++)
+            {
+                // The Next method is a part of the Random class. You can
+                // call it if you have a Random typed object, which we do.
+                int roll = randomGen.Next(6) + 1;
+                total += roll;
+
+                // We print out the individual die rolls, one at a time. 
+                // The way these are being printed out (2+3+4+6=15), I have
+                // it print out an extra plus, only if we're not on the last
+                // one.
+                if (index != count - 1)
+                {
+                    Console.Write(roll + "+");
+                }
+                else
+                {
+                    Console.Write(roll);
+                }
+            }
+
+            // Print the sum.
+            Console.WriteLine("=" + total);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandomClass/RandomClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGen = new Random(); // this is also known as a constructor
+
+            int aRandomNumber = randomGen.Next(6) + 1; // Add one, because Next(6) gives us 0 to 5.
+        }
+    }
+}
