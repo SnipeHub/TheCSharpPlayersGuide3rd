73a06be5904e504acc62e8620f032189b0777612
SnipeHub <modmapper@hotmail.com>
2018-10-14 15:25:43 +1100

    Classes chp18
    -creating classes

---
+++ CreatingClasses/CreatingClasses/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingClasses
+{
+    class Book
+    {
+        // When creating variables in a class, you can't use var, you must specify the type. These are known as Instance Variables.
+        // these instance variables can be accessed by any constructor
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        // creating our constructor. They look similar to a method, the difference is that the constructor must share the same name as the class, a normal method is forbidden.
+        // This is, in fact, the way that the C# compiler distinguishes the two.
+        // The second difference is that constructors do not (and cannot) specify a return type. Not even void. By contrast, methods are required to specify a return type.
+        public Book(string title, string author) // Making this public, allows it to be accessed from outside of the class. Private would make it reachable from inside the class.
+        {
+            this.title = title;
+            this.author = author;
+        }
+        // A good way to name your variables so you don't get confused is to name your class level instance variables with either a 'm_' or just a plain '_' underscore
+
+        // Multiple constructors. Adding a constructor that allows the user to additionally specify the pages and word count.
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+    }
+}
---
+++ VariableScope/VariableScope/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableScope
+{
+    class Program
+    {
+        public void DoSomething()
+        {
+            int a = 0;
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            x = -5; // This won't compile--the variable 'x' is "out of scope".
+        }
+
+        /// <summary>
+        /// Interestingly, we can use the same variable named (x) for a different variable in a second for loop and they will not conflict.
+        /// </summary>
+        public void DoMore()
+        {
+            for (int x = 0; x < 10; x++)
+                Console.WriteLine(x);
+
+            for (int x = 50; x < 60; x++)
+                Console.WriteLine(x);
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
