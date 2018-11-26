06876e6ab7fec03ff2dab551f8a21d5093054f74
SnipeHub <modmapper@hotmail.com>
2018-11-26 20:01:58 +1100

    Properties part 2
    Chp 19 complete

--- Properties/Properties/Program.cs
+++ Properties/Properties/Program.cs
@@ -21,13 +21,16 @@ namespace Properties
             {
                 return score;
             }
-            set
+            private set // This can now be changed only from within this class.
             {
                 score = value;
                 if (score < 0)
                     score = 0;
             }
         }
+
+        // another way to do the above is like so. This is known as an auto-implemented property.
+        public int Score { get; set; }
     }
 
     class Time
@@ -53,5 +56,29 @@ namespace Properties
                 return seconds / 60; // But there's no 'minutes' backing field here.
             }
         }
+    }
+
+    /// <summary>
+    /// Setting the get only makes it writable while the object is being created, but not afterwards.
+    /// </summary>
+    public class Vector2 // Read-only auto-implemented properties
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Vector2(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    /// <summary>
+    /// setting default values. The default value is assigned before the constructor begins, and setting a new value in the constructor will overwrite the default value.
+    /// </summary>
+    public class Vector3
+    {
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0;
     }
 }
